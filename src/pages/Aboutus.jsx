85c7944941a79261b9aa7d2f17d7932ce2fede9d
Ajayatwrk <151567625+Ajayatwrk@users.noreply.github.com>
2025-03-03 21:57:17 +0530

    Update Aboutus.jsx

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import companyLogo from "../assets/logo.png";
-import profile from "../assets/profile.png";
+import profile from "../assets/founder.jpeg";
 import health from "../assets/health.png";
 import amet from "../assets/amet.png";
 import health1 from "../assets/health1.png";
